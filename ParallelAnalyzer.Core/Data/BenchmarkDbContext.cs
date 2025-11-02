e0480fac9250da2cc225a1fd44bed92f423f80a3
Pascal100500 <kingandbuffon@gmail.com>
2025-11-02 22:14:12 +0300

    Добавление папки Data

---
+++ ParallelAnalyzer.Core/Data/BenchmarkDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ParallelAnalyzer.Core.Models;
+
+namespace ParallelAnalyzer.Core.Data
+{
+    public class BenchmarkDbContext : DbContext
+    {
+        static BenchmarkDbContext()
+        {
+            using var db = new BenchmarkDbContext(skipEnsure: true);
+            db.Database.EnsureCreated();
+        }
+
+        public BenchmarkDbContext(bool skipEnsure = false)
+        {
+            // пустой параметр для подавления вызова EnsureCreated при создании из статического конструктора
+        }
+        public DbSet<SystemInfo> Systems { get; set; }              
+        public DbSet<NumberBenchmarkResult> NumberBenchmarks { get; set; } 
+        public DbSet<FileBenchmarkResult> FileBenchmarks { get; set; }
+        public DbSet<BenchmarkTask> Tasks { get; set; } = null!;
+        public DbSet<BenchmarkSession> Sessions { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Путь: <папка исполняемого файла>\Data\benchmark.db
+            var dbFolder = Path.Combine(AppContext.BaseDirectory, "Data");
+            Directory.CreateDirectory(dbFolder);
+
+            var dbPath = Path.Combine(dbFolder, "benchmark.db");
+            optionsBuilder.UseSqlite($"Data Source={dbPath}");
+        }
+    }
+}
---
+++ ParallelAnalyzer.Core/Data/DatabaseManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParallelAnalyzer.Core.Data;
+using ParallelAnalyzer.Core.Models;
+
+namespace ParallelAnalyzer.Core.Data
+{
+    public static class DatabaseManager
+    {
+        public static void Initialize()
+        {
+            using var db = new BenchmarkDbContext();
+            db.Database.EnsureCreated();
+        }
+
+        public static int SaveSystemInfo(SystemInfo system)
+        {
+            using var db = new BenchmarkDbContext();
+            db.Systems.Add(system);
+            db.SaveChanges();
+            return system.Id;
+        }
+
+        public static void SaveNumberBenchmark(NumberBenchmarkResult result)
+        {
+            using var db = new BenchmarkDbContext();
+            db.NumberBenchmarks.Add(result);
+            db.SaveChanges();
+        }
+
+        public static void SaveFileBenchmark(FileBenchmarkResult result)
+        {
+            using var db = new BenchmarkDbContext();
+            db.FileBenchmarks.Add(result);
+            db.SaveChanges();
+        }
+    }
+}
